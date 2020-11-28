cd1f14154fdbdec320c261f1f0b26cc32272069d
michelebussolotto@gmail.com <michelebussolotto@gmail.com>
2020-11-28 23:26:53 +0100

    fix

--- week2/lcm.cpp
+++ week2/lcm.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 #include <vector>
 
-unsigned long gcd_naive(unsigned long a, unsigned long b) {
-  unsigned long current_gcd = 1;
-  for (unsigned long d = 2; d <= a && d <= b; d++) {
-    if (a % d == 0 && b % d == 0) {
-      if (d > current_gcd) {
-        current_gcd = d;
-      }
-    }
-  }
-  return current_gcd;
-}
-
 unsigned long gcd(unsigned long a, unsigned long b) {
   int current_gcd = 1;
   unsigned long tmp = 0;
@@ -34,9 +22,22 @@ unsigned long gcd(unsigned long a, unsigned long b) {
 }
 
 
+long long lcm_naive(int a, int b) {
+  for (long l = 1; l <= (long long) a * b; ++l)
+    if (l % a == 0 && l % b == 0)
+      return l;
+
+  return (long long) a * b;
+}
+
+unsigned long lcm(unsigned long a, unsigned long b) {
+  return a*b/gcd(a,b);
+
+}
+
 int main() {
-  int a, b;
+  unsigned long a, b;
   std::cin >> a >> b;
-  std::cout << gcd(a, b) << std::endl;
+  std::cout << lcm(a, b) << std::endl;
   return 0;
 }
